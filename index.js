f4881cc18b2abc45d514fbce0713b3661367f7c1
Rashad <rashad.walcott15@gmail.com>
2019-08-15 09:14:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+//Creates Breakfast Class
+class Breakfast{
+  constructor(food,drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+//Creates Lunch Class
+class Lunch {
+  constructor(salad,soup,drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+//Creates Dinner Class
+class Dinner {
+  constructor(salad,soup,entree,dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+
+  }
+}
